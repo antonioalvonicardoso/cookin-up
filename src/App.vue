f58a2e9bf218da8154c795401165ea0e7e8c8494
Antonio Cardoso <antonioc_contas@hotmail.com>
2024-06-04 13:58:07 -0300

    cabecalho
    Criando o cabeçalho da home page.

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,17 @@
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+  <header class="banner">
+    <div class="apresentacao">
+      <img src="./assets/images/logo.svg" alt="Logo do Cookin' Up" class="logo">
+      <p class="cabecalho-lh frase-cabecalho">
+        <span class="texto-verde">Um banquete de ideias para</span>
+        despertar o chef que há em você!
+      </p>
+      <p class="subtitulo-lg">
+        Explore novas receitas todos os dias com os ingredientes que estão ao seu alcance!
+    </p>
     </div>
+
+    <img src="./assets/images/foto-banner.png" alt="Foto de uma mulher cozinhando com uma bacia de vidro nas mãos. Ela está sorrindo e está apoiando um celular entre seu rosto e ombro." class="foto-banner">
+
   </header>
-
-  <main>
-    <TheWelcome />
-  </main>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
